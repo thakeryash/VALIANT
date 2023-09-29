3de5756d4136b2dacb91b73ba1814f10fc4dc736
thakeryash <yashthaker8182@gmail.com>
2023-09-29 13:06:50 +0530

    code
    Code for html

---
+++ js/home.js
@@ -0,0 +1,40 @@
+
+$(document).ready(function(){
+    $('.your-class').slick({
+        infinite: false,
+        slidesToShow: 5,
+        slidesToScroll: 3,
+        arrows: true
+        // responsive: [
+        //   {
+        //     breakpoint: 800,
+        //     settings: {
+        //       slidesToShow: 3,
+        //       slidesToScroll: 3,
+        //       infinite: true,
+        //       dots: true
+        //     }
+        //   },
+        //   {
+        //     breakpoint: 600,
+        //     settings: {
+        //       slidesToShow: 2,
+        //       slidesToScroll: 2
+        //     }
+        //   },
+        //   {
+        //     breakpoint: 480,
+        //     settings: {
+        //       slidesToShow: 1,
+        //       slidesToScroll: 1
+        //     }
+        //   }
+        //   // You can unslick at a given breakpoint now by adding:
+        //   // settings: "unslick"
+        //   // instead of a settings object
+        // ]
+    });
+
+
+    
+});
